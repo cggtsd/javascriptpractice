5aa8ca1ebf7a05b8e2de76b18737b38738dff3c9
daraqushan <daraqushan@gmail.com>
2024-11-13 16:41:01 +0530

    commits

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("welcome to react js with typescript")
+// console.log("welcome to react js with typescript")
+
+import {PI,getCircumference,getArea,getVolume } from './mathUtil.js'
+
+console.log(PI)
+console.log(`Circumference is ${getCircumference(10)} cms`)
+console.log(`Area is ${getArea(10)} cms^2`)
+console.log(`Volume is ${getVolume(10)} cms^3`)
